439287a626422137824f407141a5d62ccc62df28
adilet26190 <rusya_alisherov@mail.ru>
2022-02-16 13:45:29 +0600

    Modified

--- src/main/java/com/in28minutes/learnspringboot/LearnSpringBootApplication.java
+++ src/main/java/com/in28minutes/learnspringboot/LearnSpringBootApplication.java
@@ -2,23 +2,23 @@ package com.in28minutes.learnspringboot;
 
 import com.in28minutes.learnspringboot.courses.bean.Account;
 import com.in28minutes.learnspringboot.courses.bean.Bank;
-//import com.in28minutes.learnspringboot.courses.bean.Course;
 import com.in28minutes.learnspringboot.courses.controller.atm;
-import org.springframework.beans.factory.annotation.Configurable;
-import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
+
 
 @SpringBootApplication
 public class LearnSpringBootApplication {
 
 	public static void main(String[] args) {
 
-		ConfigurableApplicationContext context = SpringApplication.run(LearnSpringBootApplication.class, args);
+		ApplicationContext context = new ClassPathXmlApplicationContext("beans.xml");
+
 		Scanner in = new Scanner(System.in);
 		Account account = new Account(12L, 12, 300);
 		Account account2 = new Account(12L, 13, 300);
@@ -28,10 +28,10 @@ public class LearnSpringBootApplication {
 		Bank b = new Bank();
 		b.setAccount(accounts);
 		atm atm1 = new atm(b);
-
-			while(true) {
-				atm1.start();
-			}
+		((ClassPathXmlApplicationContext) context).close();
+		//	while(true) {
+		//			atm1.start();
+		//		}
 
 	}
 
